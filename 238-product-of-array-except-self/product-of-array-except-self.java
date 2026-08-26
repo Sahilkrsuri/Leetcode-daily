0e5a916b7a43adc47d98944c2e4984bb19a3ed56
Sahil Suri <109021431+Sahilkrsuri@users.noreply.github.com>
2026-08-27 00:59:24 +0530

    Time: 3 ms (24.06%) | Memory: 63.3 MB (97.48%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,47 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        int n= nums.length;
+        int[] pre = new int[n];
+        int[] suf = new int[n];
 
-        int n = nums.length;
-        int product = 1;
-        int nzp = 1;
-        int zeros = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            if (nums[i] == 0) {
-                zeros++;
-            } else {
-                nzp *= nums[i];
-            }
-
-            product *= nums[i];
+        pre[0] = 1;
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i-1]*nums[i-1];
         }
 
-        // More than one zero
-        if (zeros > 1) {
-            for (int i = 0; i < n; i++) {
-                nums[i] = 0;
-            }
+        suf[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            suf[i] = suf[i + 1] * nums[i + 1];
         }
-
-        // Exactly one zero
-        else if (zeros == 1) {
-            for (int i = 0; i < n; i++) {
-                if (nums[i] == 0) {
-                    nums[i] = nzp;
-                } else {
-                    nums[i] = 0;
-                }
-            }
+        for(int i=0;i<n;i++){
+            nums[i]=pre[i]*suf[i];
         }
-
-        // No zero
-        else {
-            for (int i = 0; i < n; i++) {
-                nums[i] = product / nums[i];
-            }
-        }
-
         return nums;
     }
 }
