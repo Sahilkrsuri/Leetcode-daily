465d98bb6db4cf0cdc0644e0be5bf6dc951a02d0
Sahil Suri <109021431+Sahilkrsuri@users.noreply.github.com>
2026-09-06 16:03:26 +0530

    Time: 4 ms (44.62%) | Memory: 44.5 MB (53.84%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,28 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int n= s.length();
+        int n = s.length();
         StringBuilder str = new StringBuilder();
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            if(ch>=97 && ch<=122){
-                str.append(ch);
-            }
-            else if(ch>=65 && ch<=90){
-                str.append((char)(ch+32));
-            }
-            else if(ch>=48 && ch<=57){
-                str.append(ch);
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                str.append(Character.toLowerCase(ch));
             }
         }
         return checkPalindrome(str.toString());
     }
-    public boolean checkPalindrome(String s){
-        int n= s.length();
-        if(n<1) return true;
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
+
+    public boolean checkPalindrome(String s) {
+        int n = s.length();
+        if (n < 1)
+            return true;
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
             i++;
